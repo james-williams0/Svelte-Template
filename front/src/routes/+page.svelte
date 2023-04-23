6b76709bb98a66b1cfdb2fd4232bdf1329eafdf1
James Williams <9088026+james-williams0@users.noreply.github.com>
2023-04-24 00:49:31 +0100

    Remove stuff

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -11,7 +11,6 @@
 </script>
 
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <button on:click={IncrementCount}>CLICK ME</button>
 <button on:click={DecrementCount}>do not click me</button>
 <p>{count}</p>
