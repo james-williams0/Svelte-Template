4db784414b0cf114e2ab3d5d24fe501eff5c0cc4
James Williams <9088026+james-williams0@users.noreply.github.com>
2023-04-25 23:28:46 +0100

    Begone single quotes and tab characters

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,16 +1,5 @@
 <script lang="ts">
-    let count: number = 0;
 
-    function IncrementCount() {
-        count++;
-    }
-
-    function DecrementCount() {
-        count--;
-    }
 </script>
 
 <h1>Welcome to SvelteKit</h1>
-<button on:click={IncrementCount}>CLICK ME</button>
-<button on:click={DecrementCount}>do not click me</button>
-<p>{count}</p>
